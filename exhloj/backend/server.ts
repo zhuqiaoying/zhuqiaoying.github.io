1f5d4617a85519916040b24a62446bc8b5376a2a
Qi Zhu <115549630@qq.com>
2023-11-17 09:10:23 +0000

    feat(backend): init score-query module

--- exhloj/backend/server.ts
+++ exhloj/backend/server.ts
@@ -1,15 +1,13 @@
 import { serve } from "https://deno.land/std@0.207.0/http/server.ts"
+import queryScore from "./modules/score-query/index.ts"
 
 function matchPage(method, route, request) {
     return request.method == method && new URLPattern({ pathname: route }).exec(request.url)
 }
 
-serve((request) => {
-    if (matchPage('GET', '/api/score-query/query', request))
-        return new Response(
-            JSON.stringify({ a: 1 }),
-            { headers: { "content-type": "application/json" } },
-        )
+serve(async (request) => {
+    if (matchPage('POST', '/api/score-query', request))
+        return await queryScore(request)
     return new Response(
         "Not found",
         { status: 404 },
