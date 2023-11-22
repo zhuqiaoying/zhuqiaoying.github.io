43cec56d2d5a56a8b5fdf50ff8d8e704c08d9377
Qi Zhu <115549630@qq.com>
2023-11-22 07:51:46 +0000

    fix(server): log request

--- exhloj/modules/score-query/index.js
+++ exhloj/modules/score-query/index.js
@@ -12,10 +12,17 @@ atPlugin(
     async () => {
         const cookie = await getCookieSid()
         const rid = /^\/record\/([0-9a-f]{24})/i.exec(window.page_url)[1]
-        const { rdoc, tdoc } = await send_hydro_get(`/d/${domainId}/record/${rid}`)
+        const { rdoc, tdoc } = await send_hydro_get(`/d/${window.domainId}/record/${rid}`)
         if (rdoc.uid !== window.user._id && typeof rdoc.uid === 'number') return
         if (!tdoc || tdoc.rule !== 'oi' || rdoc.testCases) return
-        console.log('test')
+        console.log(await send_post(`${__exhloj_server_url}/api/score-query`, {
+            url: window.location.origin,
+            username: window.user.uname,
+            cookie,
+            domain: window.domainId,
+            contestId: tdoc._id,
+            pid: '1',
+        }))
     },
     () => document.querySelector('html').getAttribute('data-page') === 'record_detail',
 )
